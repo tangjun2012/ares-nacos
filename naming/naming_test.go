54d5046639769f11cfd8708690fc5b42cfffbe1e
iqinning <q383175101@163.com>
2019-08-12 19:01:30 +0800

    add test

---
+++ naming/naming_test.go
@@ -0,0 +1,12 @@
+package naming_test
+
+import (
+	"github.com/swift9/ares-nacos/naming"
+	"testing"
+)
+
+func TestRegisterService(t *testing.T) {
+	isSuccess, err := naming.RegisterService("localhost", 8080, "test", "", map[string]string{})
+	println(isSuccess, err)
+
+}
