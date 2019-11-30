cc2d28ccd3746303ee82ed59f0463f54c48c7b37
iqinning <q383175101@163.com>
2019-11-30 11:28:12 +0800

    local env

--- config/config_test.go
+++ config/config_test.go
@@ -3,8 +3,6 @@ package config_test
 import (
 	"github.com/swift9/ares-nacos/config"
 	"os"
-	"regexp"
-	"strings"
 	"testing"
 )
 
@@ -14,31 +12,4 @@ func TestEnv(t *testing.T) {
 
 func TestConfigString(t *testing.T) {
 	println(config.GetString("test", "test"))
-}
-
-func TestGetVal(t *testing.T) {
-	s := "a ${s.d  },${s.dd}"
-	reg := regexp.MustCompile(`\$\{(\s+)?(\S)+(\s+)?\}`)
-	i := reg.FindAllString(s, -1)
-	for _, p := range i {
-		regexp.MustCompile(`\$\{(\s+)?(\S)+(\s+)?\}`)
-		s = strings.ReplaceAll(s, p, "88")
-	}
-	println(s)
-}
-
-func TestGetVal2(t *testing.T) {
-	s := "${s1.s.s.a_d  }"
-	reg := regexp.MustCompile(`(\w+(\.)?)+`)
-	i := reg.FindAllString(s, -1)
-	println(i)
-}
-
-func TestGetVal3(t *testing.T) {
-	println(config.GetValue("1.${test}"))
-}
-
-func TestGetVal4(t *testing.T) {
-	println(config.GetString("a.b"))
-	println(config.GetString("a.c"))
 }
---
+++ config/local.go
@@ -0,0 +1,103 @@
+package config
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"sync"
+)
+
+type localEnv struct {
+	local *sync.Map
+}
+
+var env *localEnv
+
+func (l *localEnv) Get(key string) string {
+	if l.local == nil {
+		return ""
+	}
+	if val, ok := l.local.Load(key); ok {
+		return val.(string)
+	}
+	return ""
+}
+
+func (l *localEnv) Set(key string, val string) {
+	if l.local == nil {
+		l.local = &sync.Map{}
+	}
+	l.local.Store(key, val)
+
+	refreshLocalFile(l.local)
+}
+
+func (l *localEnv) Delete(key string) {
+	if l.local == nil {
+		return
+	}
+	l.local.Delete(key)
+	refreshLocalFile(l.local)
+}
+
+func getLocalFile() string {
+	home, _ := os.UserHomeDir()
+	return home + "/" + GetString("local", "ares.env")
+}
+
+var fileLock = sync.Mutex{}
+
+func refreshLocalFile(l *sync.Map) {
+	fileLock.Lock()
+	defer fileLock.Unlock()
+	configFile := getLocalFile()
+	os.Remove(configFile)
+
+	m := map[string]string{}
+
+	l.Range(func(key, value interface{}) bool {
+		m[key.(string)] = value.(string)
+		return true
+	})
+	bs, _ := json.Marshal(m)
+
+	ioutil.WriteFile(configFile, bs, os.ModePerm)
+}
+
+func loadEnv() {
+	if env != nil {
+		return
+	}
+	configFile := getLocalFile()
+
+	_, err := os.Stat(configFile)
+	if err != nil {
+		env = &localEnv{
+			local: &sync.Map{},
+		}
+		return
+	}
+
+	bs, _ := ioutil.ReadFile(configFile)
+
+	m := map[string]string{}
+	json.Unmarshal(bs, &m)
+
+	var local = sync.Map{}
+	for key, val := range m {
+		local.Store(key, val)
+	}
+
+	env = &localEnv{
+		local: &local,
+	}
+	return
+}
+
+func init() {
+	loadEnv()
+}
+
+func GetLocal() *localEnv {
+	return env
+}
